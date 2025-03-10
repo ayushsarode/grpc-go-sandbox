87f2f452b844b33b544ef23278ddd6bf4cd6dba4
ayushsarode <ayushsarode777@gmail.com>
2025-03-11 00:23:37 +0530

    bidirectional stream fixed

--- client/bi_stream.go
+++ client/bi_stream.go
@@ -10,45 +10,39 @@ import (
 )
 
 func callSayHelloBidirectionalStream(client pb.GreetServiceClient, names *pb.NameList) {
-
-
-	log.Printf("Bidirectional streaming started")
-
+	log.Printf("Bidirectional Streaming started")
 	stream, err := client.SayHelloBidirectionalStreaming(context.Background())
-
-	if err != nil  {
-		log.Fatalf("could not send names: %v", err)
+	if err != nil {
+		log.Fatalf("Could not send names: %v", err)
 	}
 
-	 waitc := make(chan struct{})
+	waitc := make(chan struct{})
 
-	 go func() {
+	go func() {
 		for {
 			message, err := stream.Recv()
 			if err == io.EOF {
 				break
 			}
-
 			if err != nil {
-				log.Fatalf("Error while Streaming %v", err)
+				log.Fatalf("Error while streaming %v", err)
 			}
 			log.Println(message)
 		}
 		close(waitc)
-	 }()
+	}()
 
-
-	 for _, name := range names.Names {
-		req := &pb.HelloRequest {
+	for _, name := range names.Names {
+		req := &pb.HelloRequest{
 			Name: name,
 		}
 		if err := stream.Send(req); err != nil {
 			log.Fatalf("Error while sending %v", err)
 		}
 		time.Sleep(2 * time.Second)
-	 }
-	 stream.CloseSend()
-	 <-waitc
-	 log.Printf("Bidirectional streaming finished")
-}
+	}
 
+	stream.CloseSend()
+	<-waitc
+	log.Printf("Bidirectional Streaming finished")
+}
